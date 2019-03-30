0704f01deae97f52e935cb8624bb47a3a631afeb
prokopparuzek <prokop.paruzek@paruzkovi.cz>
2019-03-30 20:19:38 +0100

    0.3.1 sjednoceni

--- letaky.c
+++ letaky.c
@@ -2,7 +2,48 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
-#include "bludiste.h"
+
+#define WALL -1
+
+#define DISCARD steps[i] = -1; continue
+
+typedef struct {
+    int depth;
+    int parent;
+} step;
+
+typedef struct {
+    int size_x;
+    int size_y;
+    int indexAc;
+    int indexFu;
+    step *buff;
+    unsigned *stackAc;
+    unsigned *stackFu;
+    int *route;
+} buffer_t;
+
+typedef struct {
+    int size_x;
+    int size_y;
+    int *bludiste;
+} blud;
+
+void (*fav)(buffer_t*, int, int*);
+
+blud storeBlud(void);
+void printBlud(blud*);
+void solve(blud *maze, int route[4]);
+void initBuff(buffer_t *buff, int*);
+int makeSteps(buffer_t *queue, blud *maze, int*);
+int solveStep(buffer_t *queue, blud *maze, int index, int*);
+void findRoute(buffer_t *queue, blud *maze, int*);
+int addWall(blud *maze, unsigned*);
+void makeMaze(unsigned size_x, unsigned size_y, unsigned lenght);
+int addWallVertical(blud *maze, unsigned*);
+unsigned makeWall(blud *maze, unsigned, int);
+unsigned makeWallVertical(blud *maze, unsigned last, int);
+void color(blud *maze);
 
 void solve(blud *maze, int route[4]) {
     if (route[0] == route[2] && route[1] == route[3]) { // Pokud je cíl stejný jako start, přeskoč
